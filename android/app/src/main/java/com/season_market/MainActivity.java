d6e3fa166e2e0a7f1e8adc6798af35377bf95c39
elmurza <Elmurza09@yandex.ru>
2023-04-20 14:31:57 +0300

    замена на kchr_market

--- android/app/src/main/java/com/season_market/MainActivity.java
+++ android/app/src/main/java/com/season_market/MainActivity.java
@@ -1,4 +1,4 @@
-package com.season_market;
+package com.kchr_market;
 
 import android.os.Bundle;
 import com.facebook.react.ReactActivity;
--- android/app/src/main/java/com/season_market/MainApplication.java
+++ android/app/src/main/java/com/season_market/MainApplication.java
@@ -1,4 +1,4 @@
-package com.season_market;
+package com.kchr_market;
 
 import android.app.Application;
 import android.content.Context;
@@ -62,7 +62,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.season_market.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.kchr_market.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
